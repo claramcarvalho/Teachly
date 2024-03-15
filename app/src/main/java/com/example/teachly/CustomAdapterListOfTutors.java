064cf1c79ec62d465dbc779a550933d345b3acd9
claramcarvalho <claramcarvalho@gmail.com>
2024-03-15 17:31:59 -0400

    Showing list of classes when searching a new class - Student

--- app/src/main/java/com/example/teachly/CustomAdapterListOfTutors.java
+++ app/src/main/java/com/example/teachly/CustomAdapterListOfTutors.java
@@ -11,27 +11,35 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.example.teachly.Classes.Class;
+import com.example.teachly.Classes.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import java.sql.Blob;
+import java.util.ArrayList;
 
 public class CustomAdapterListOfTutors extends BaseAdapter {
 
     Context context;
-    String photos[];
-    String names[], emails[], phones[];
-
+    ArrayList<Class> list;
+    String nameOfTeacher, teacherEmail, teacherPhone;
     LayoutInflater inflater;
 
-    public CustomAdapterListOfTutors (Context appContext, String[] photos, String[] names, String[] emails, String[] phones) {
+    public CustomAdapterListOfTutors (Context appContext, ArrayList<Class> list) {
         context = appContext;
-        this.photos = photos;
-        this.names = names;
-        this.emails = emails;
-        this.phones = phones;
+        this.list = list;
         inflater = LayoutInflater.from(appContext);
     }
     @Override
     public int getCount() {
-        return names.length;
+        return list.size();
     }
 
     @Override
@@ -47,14 +55,39 @@ public class CustomAdapterListOfTutors extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = inflater.inflate(R.layout.list_item_tutors,null);
-        ImageView photo = convertView.findViewById(R.id.photoTutorListView);
+        TextView className = convertView.findViewById(R.id.txtClassNameListView);
+        TextView classCategory = convertView.findViewById(R.id.txtClassTypeListView);
         TextView name = convertView.findViewById(R.id.txtTutorNameListView);
         TextView email = convertView.findViewById(R.id.txtTutorEmailListView);
         TextView phone = convertView.findViewById(R.id.txtTutorPhoneListView);
 
-        name.setText(this.names[position]);
-        email.setText(this.emails[position]);
-        phone.setText(this.phones[position]);
+        className.setText(this.list.get(position).getName());
+        classCategory.setText(this.list.get(position).getCategory().name());
+
+        String teacherId = this.list.get(position).getTeacherUId();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query findTutor = reference.orderByChild("uId").equalTo(teacherId);
+
+        findTutor.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    nameOfTeacher = snapshot.child("fullName").getValue(String.class);
+                    teacherEmail = snapshot.child("email").getValue(String.class);
+                    teacherPhone = snapshot.child("phoneNumber").getValue(String.class);
+
+                    name.setText(nameOfTeacher);
+                    email.setText(teacherEmail);
+                    phone.setText(teacherPhone);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return convertView;
     }
--- app/src/main/java/com/example/teachly/EditClass.java
+++ app/src/main/java/com/example/teachly/EditClass.java
@@ -1,7 +1,11 @@
 package com.example.teachly;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
@@ -17,6 +21,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.teachly.Classes.EnumCategoryClass;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +34,14 @@ import java.util.List;
 public class EditClass extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView btnSave, btnDelete;
-    String classId, className, classDescription, classCategory, classColor;
+    String classId, className, classDescription, classCategory, classColor, newClassCategory;
+    EditText edtClassName, edtDescription;
+    Spinner spinner;
+    RadioGroup radioGroup;
+    String txtNewColor;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,17 +52,20 @@ public class EditClass extends AppCompatActivity implements AdapterView.OnItemSe
 
         classId = getIntent().getStringExtra("editClassId");
 
-        EditText edtClassName = findViewById(R.id.edtEditClassName);
+        edtClassName = findViewById(R.id.edtEditClassName);
         className = getIntent().getStringExtra("editClassName");
         edtClassName.setText(className);
 
-        EditText edtDescription = findViewById(R.id.edtEditClassDescription);
+        edtDescription = findViewById(R.id.edtEditClassDescription);
         classDescription = getIntent().getStringExtra("editClassDescription");
         edtDescription.setText(classDescription);
 
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("classes");
+
         ////// CATEGORIA
         classCategory = getIntent().getStringExtra("editClassCategory");
-        Spinner spinner = findViewById(R.id.spinnerTeacherEditClassCategory);
+        spinner = findViewById(R.id.spinnerTeacherEditClassCategory);
         List<String> categories = new ArrayList<String>();
         Integer selectedIndex = 0;
         for (EnumCategoryClass value : EnumCategoryClass.values()){
@@ -68,38 +88,146 @@ public class EditClass extends AppCompatActivity implements AdapterView.OnItemSe
         edtClassColor.setColorFilter(Color.parseColor(classColor), PorterDuff.Mode.SRC_IN);
 
         /////CHANGING COLOR IF RADIOBUTTON IS SELECTED
-        RadioGroup radioGroup = findViewById(R.id.radioColorEditClass);
+        radioGroup = findViewById(R.id.radioColorEditClass);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton radioButton = findViewById(checkedId);
-                String text = radioButton.getText().toString();
-                edtClassColor.setColorFilter(Color.parseColor(text), PorterDuff.Mode.SRC_IN);
+                txtNewColor = radioButton.getText().toString();
+                edtClassColor.setColorFilter(Color.parseColor(txtNewColor), PorterDuff.Mode.SRC_IN);
             }
         });
+
+        ///////CHANGING COLOR OF RADIO BUTTON
+        for (int i = 0; i<radioGroup.getChildCount() ; i++) {
+            View view = radioGroup.getChildAt(i);
+            if (view instanceof RadioButton) {
+                RadioButton radioButton = (RadioButton) view;
+                String textRadioButton = radioButton.getText().toString();
+                if (textRadioButton.equals(classColor)) {
+                    radioButton.setChecked(true);
+                    break;
+                }
+            }
+        }
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (isClassNameChanged()) {
+                    Toast.makeText(EditClass.this, "Class Name was updated!", Toast.LENGTH_SHORT).show();
+                }
+                if (isClassDescriptionChanged()) {
+                    Toast.makeText(EditClass.this, "Class Description was updated!", Toast.LENGTH_SHORT).show();
+                }
+                if (isClassCategoryChanged()) {
+                    Toast.makeText(EditClass.this, "Class Category was updated!", Toast.LENGTH_SHORT).show();
+                }
+                if (isClassColorChanged()) {
+                    Toast.makeText(EditClass.this, "Class Color was updated!", Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(EditClass.this, HomeTeacher.class);
+                startActivity(intent);
+                finish();
             }
         });
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(EditClass.this);
+                builder.setTitle("Delete Class");
+                builder.setMessage("Are you sure you want to delete this class?\nThis action cannot be undone!");
+                builder.setPositiveButton("Delete Account", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                        Query findClass = reference.orderByChild("classId").equalTo(classId);
+
+                        findClass.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    snapshot.child(classId).getRef().removeValue();
+
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(EditClass.this);
+                                    builder.setTitle("Class deleted");
+                                    builder.setMessage("Your class " + className + " was deleted!!");
+
+                                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(EditClass.this, HomeTeacher.class);
+                                            startActivity(intent);
+                                            finish();
+                                            dialog.dismiss();
+                                        }
+                                    });
+                                    builder.setIcon(R.drawable.icon_check_30);
+                                    builder.setCancelable(false);
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+                                } else {
+                                    Toast.makeText(EditClass.this, "Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+                });
+                builder.setIcon(android.R.drawable.ic_delete);
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.setCancelable(false);
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        newClassCategory = parent.getItemAtPosition(position).toString();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    private boolean isClassNameChanged (){
+        if (!className.equals(edtClassName.getText().toString().trim())) {
+            reference.child(classId).child("name").setValue(edtClassName.getText().toString().trim());
+            return true;
+        }
+        return false;
+    }
+    private boolean isClassDescriptionChanged (){
+        if (!classDescription.equals(edtDescription.getText().toString().trim())) {
+            reference.child(classId).child("description").setValue(edtDescription.getText().toString().trim());
+            return true;
+        }
+        return false;
+    }
+    private boolean isClassCategoryChanged (){
+        if (!classCategory.equals(newClassCategory)) {
+            reference.child(classId).child("category").setValue(newClassCategory);
+            return true;
+        }
+        return false;
+    }
+    private boolean isClassColorChanged (){
+        if (!classColor.equals(txtNewColor)) {
+            reference.child(classId).child("color").setValue(txtNewColor);
+            return true;
+        }
+        return false;
     }
 }
